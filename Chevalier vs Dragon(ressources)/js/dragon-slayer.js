86d2cff0ba7cadb47e612c03d6e0add77a40520b
Thomas <thomas.degardin@ynov.com>
2026-04-16 10:19:34 +0200

    Update dragon-slayer.js

--- Chevalier vs Dragon(ressources)/js/dragon-slayer.js
+++ Chevalier vs Dragon(ressources)/js/dragon-slayer.js
@@ -84,6 +84,71 @@ function jouerPartie() {
     }
 }
 
+
+/**
+ * Affiche l'état du jeu avec le bonus des images amochées (Bonus n1)
+ */
+function afficherEtatDuJeu() {
+    document.write('<div class="game-state">');
+    
+    // --- CHEVALIER ---
+    document.write('<figure class="game-state_player">');
+        // Bonus 1 : Si PV < 30%, on change l'image
+        // On considère que 100 PV est la base (ou on simplifie pour le moment)
+        if (pvJoueur < 30) {
+            document.write('<img src="images/knight-wounded.png" alt="Chevalier amoché">');
+        } else {
+            document.write('<img src="images/knight.png" alt="Chevalier">');
+        }
+        
+        // Si le joueur est mort, on écrit Game Over, sinon ses PV
+        if (pvJoueur <= 0) {
+            document.write('<figcaption>Game Over</figcaption>');
+        } else {
+            document.write('<figcaption>' + pvJoueur + ' PV</figcaption>');
+        }
+    document.write('</figure>');
+
+    // --- DRAGON ---
+    document.write('<figure class="game-state_player">');
+        if (pvDragon < 30) {
+            document.write('<img src="images/dragon-wounded.png" alt="Dragon amoché">');
+        } else {
+            document.write('<img src="images/dragon.png" alt="Dragon">');
+        }
+
+        if (pvDragon <= 0) {
+            document.write('<figcaption>Vaincu !</figcaption>');
+        } else {
+            document.write('<figcaption>' + pvDragon + ' PV</figcaption>');
+        }
+    document.write('</figure>');
+    
+    document.write('</div>');
+}
+
+/**
+ * Affiche l'écran final de victoire ou défaite
+ */
+function afficherFinDePartie() {
+    document.write('<footer>');
+    document.write('<h3>Fin de la partie</h3>');
+    document.write('<figure class="game-end">');
+
+    if (pvJoueur > 0) {
+        // Le joueur a gagné
+        document.write('<figcaption>Vous avez terrassé le dragon ! Félicitations Chevalier !</figcaption>');
+        document.write('<img src="images/knight-winner.png" alt="Chevalier vainqueur">');
+    } else {
+        // Le dragon a gagné
+        document.write('<figcaption>Vous avez perdu le combat, le dragon vous a carbonisé !</figcaption>');
+        document.write('<img src="images/dragon-winner.png" alt="Dragon vainqueur">');
+    }
+
+    document.write('</figure>');
+    document.write('</footer>');
+}
+
 /*************************************************************************************************/
 /* ************************************** CODE PRINCIPAL *************************************** */
 /*************************************************************************************************/
@@ -110,3 +175,6 @@ afficherEtatDuJeu();
 
 // 4. On lance le combat !
 jouerPartie();
+
+// 5. Une fois que la boucle est finie, on affiche le résultat final
+afficherFinDePartie();
