0626c9161eb8d5059fd957ead373fd1fab125843
Thomas <thomas.degardin@ynov.com>
2026-04-16 10:09:09 +0200

    Update dragon-slayer.js

--- Chevalier vs Dragon(ressources)/js/dragon-slayer.js
+++ Chevalier vs Dragon(ressources)/js/dragon-slayer.js
@@ -35,6 +35,54 @@ function afficherEtatDuJeu() {
     document.write('</div>');
 }
 
+/**
+ * Détermine qui attaque et calcule les dégâts
+ */
+function lancerTourDeCombat(numeroTour) {
+    // 1. On affiche le numéro du tour
+    document.write('<h3>Tour n°' + numeroTour + '</h3>');
+
+    // 2. Calcul de l'initiative (10D6 chacun)
+    let initiativeJoueur = throwDices(10, 6);
+    let initiativeDragon = throwDices(10, 6);
+    
+    // Dégâts de base : 3D6
+    let degats = throwDices(3, 6);
+
+    // 3. Comparaison de l'initiative
+    if (initiativeJoueur > initiativeDragon) {
+        // Le joueur gagne l'initiative
+        pvDragon = pvDragon - degats;
+        
+        document.write('<figure class="game-round">');
+            document.write('<img src="images/knight-winner.png" alt="Chevalier vainqueur">');
+            document.write('<figcaption>Vous êtes plus rapide, vous infligez ' + degats + ' dégâts !</figcaption>');
+        document.write('</figure>');
+    } 
+    else {
+        // Le dragon gagne l'initiative
+        pvJoueur = pvJoueur - degats;
+
+        document.write('<figure class="game-round">');
+            document.write('<img src="images/dragon-winner.png" alt="Dragon vainqueur">');
+            document.write('<figcaption>Le dragon est plus rapide, il vous inflige ' + degats + ' dégâts !</figcaption>');
+        document.write('</figure>');
+    }
+}
+
+/**
+ * Lance la boucle principale du jeu
+ */
+function jouerPartie() {
+    let tour = 1;
+
+    // Tant que tout le monde est en vie (PV > 0)
+    while (pvDragon > 0 && pvJoueur > 0) {
+        lancerTourDeCombat(tour);
+        afficherEtatDuJeu();
+        tour++;
+    }
+}
 
 /*************************************************************************************************/
 /* ************************************** CODE PRINCIPAL *************************************** */
@@ -60,3 +108,5 @@ if (niveau === 1) {
 document.write('<h2>Que la fête commence !!</h2>');
 afficherEtatDuJeu();
 
+// 4. On lance le combat !
+jouerPartie();
